67ac3ef431344f8620b6646c67516512a6509f90
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-08-23 12:31:45 +0200

    Added classes in loan.py to manage books loans and books reservations.
    Edited class Book - information about book's status and who borrowed is available in LoanedBook class.

--- models/book.py
+++ models/book.py
@@ -1,18 +1,17 @@
 # book class with atrbutes and methods
 
 class Book:
-    def __init__(self, author, title, year, is_borrowed=False, borrowed_by=None):
+    def __init__(self, author, title, year):
         self.author = author
         self.title = title
         self.year = year
-        self.is_borrowed = is_borrowed
-        self.borrowed_by = borrowed_by
+
 
 class BookCategory:
     def __init__(self, name):
         self.name = name
         self.books = []
-        
+
     def add_book(self, book):
         self.books.append(book)
 
--- models/loan.py
+++ models/loan.py
@@ -1,1 +1,27 @@
 # loan class with attributes and methods (who borrowed the book, when it was borrowed, when it was returned)
+
+from datetime import datetime
+from models.book import Book
+
+class LoanedBook:
+    def __init__(self, book : Book, borrowed_by, is_borrowed=False, loan_date=None, return_date=None):
+        self.book = book
+        self.borrowed_by = borrowed_by
+        self.is_borrowed = is_borrowed
+        self.loan_date = loan_date or datetime.now()
+        self.return_date = return_date
+
+    def borrow_book(self):
+        if not self.is_borrowed:
+            self.is_borrowed = True
+            self.loan_date = datetime.now()
+            self.return_date = None
+        else:
+            raise Exception("Book is already borrowed") # ADD this exception to module exceptions.py and import here
+
+class BookReservation:
+    def __init__(self, book : Book, reserved_by, reservation_date=None):
+        self.book = book
+        self.reserved_by = reserved_by
+        self.reservation_date = reservation_date
+
