ce8ffdd77cca7dc7b04d864836a80aa5d98a56ea
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-08-24 11:12:15 +0200

    Added class User in User. module.
    Default user role is "reader"

--- models/user.py
+++ models/user.py
@@ -1,1 +1,10 @@
 # class user to represent a user in te library or administrator
+
+class User():
+    def __init__(self, user_id, email, role="reader"):
+        self.user_id = user_id
+        self.email = email
+        self.role = role
+
+    def __repr__(self):
+        return f"User id: {self.user_id}, user email: {self.email}, user role: {self.role}"
