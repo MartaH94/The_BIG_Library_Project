22efd2f9af06df50a10ff1716b738af30ebb039a
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-08-24 12:14:09 +0200

    Refactored user permission validation logic in validators.py

--- utils/validators.py
+++ utils/validators.py
@@ -1,1 +1,25 @@
 # functions to validate user inputs, eg. login, credentials, etc.
+
+from models.user import User
+
+user_permissions = {
+    "reader" : {
+        "borrow_book" : True,
+        "return_book" : True,
+        "view_books" : True,
+        "manage_books" : False,
+        "manage_users" : False
+    },
+    "admin": {
+        "borrow_book" : False,
+        "return_book" : False,
+        "view_books" : True,
+        "manage_books" : True,
+        "manage_users" : True
+    }
+}
+
+def has_permission(user, action):
+    role = user.role
+    return user_permissions.get(role, {}).get(action, False)
+
