4366ca0adf8a1d75390b15ac3683bc29ce62efe3
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-09-09 20:02:28 +0200

    Refactor test cases in TestBookService: enhance readability and consistency in print statements, and improve book addition and search tests

--- tests/test_book_service.py
+++ tests/test_book_service.py
@@ -21,96 +21,82 @@ class TestBookService(unittest.TestCase):
         self.admin_service = BookService(self.admin_authorisation)
         self.reader_service = BookService(self.reader_authorisation)
 
-        self.book = Book(author="Adam Mickiewicz", title="Pan Tadeusz", year=1834)
-        print("\n[SETUP] Creating users and BookService instances for this test.")
+        self.book_service = BookService(self.admin_authorisation)
+        self.book_service.books = []
+        self.book_service.books.append(Book(author="J.K. Rowling", title="Harry Potter and the Philosopher's Stone", year=1997))
+        self.book_service.books.append(Book(author="George Orwell", title="1984", year=1949))
+        self.book_service.books.append(Book(author="J.R.R. Tolkien", title="The Hobbit", year=1937))
+        self.book_service.books.append(Book(author="F. Scott Fitzgerald", title="The Great Gatsby", year=1925))
 
+        self.book = Book(author="Sample Author", title="Sample Title 2020", year=2020)
 
+        self.admin_service.books = self.book_service.books.copy()
+        self.reader_service.books = self.book_service.books.copy()
 
-# [TESTS FOR: ADDING BOOK]       
- 
+# [TESTS FOR: ADDING BOOK]        
     def test_add_book(self):
-        print("\n[TEST CASE] test_add_book: Adding new book.")
-        self.admin_service.add_book(self.book)
-        self.assertIn(self.book, self.admin_service.books)
-        print(f"[PASS] test_add_book: Book '{self.book.title}' added succesfully.")
+        print("\n[TEST CASE FOR: ADDING BOOK] test_add_book: Adding new book.")
+        book_to_add = self.book_service.books[0]
+        self.admin_service.add_book(book_to_add)
+        self.assertIn(book_to_add, self.admin_service.books)
+        print(f"[PASS] test_add_book: Book '{book_to_add.title}' added succesfully.")
         
     def test_add_book_without_author_or_title(self):
-        print("\n[TEST CASE] test add book with empty data (author or title): Adding book with empty author or title... ")
+        print("\n[TEST CASE FOR: ADDING BOOK] add book with empty data (author or title): Adding book with empty author or title... ")
         self.assertRaises(exc.InvalidBookDataError, self.admin_service.add_book, Book(author=None, title=None, year=2020))
         print(f"[PASS] test add book with empty data (author or title): Exception {exc.InvalidBookDataError} raised properly.")   
         
 # [TESTS FOR: EDIT BOOK]
     def test_user_has_no_permission_to_edit_book(self):
-        print("\nTEST CASE] test_user_has_no_permission_to_edit_book: User role is 'reader', trying to edit book...")
-        self.reader_service.books.append(self.book)
-        self.assertRaises(exc.PermissionError, self.reader_service.edit_book, self.book, title="New Title")
+        print("\nTEST CASE FOR: EDIT BOOK] user_has_no_permission_to_edit_book: User role is 'reader', trying to edit book...")
+        book_to_edit = self.book_service.books[1]
+        self.assertRaises(exc.PermissionError, self.reader_service.edit_book, book_to_edit, title="New Title")
         print(f"[PASS] test_user_has_no_permission_to_edit_book: Exception {exc.PermissionError} raised properly.")
 
-
     def test_edit_books_title(self):
-        print("\n[TEST CASE] test_edit_books_title: Adding book and editing its title.") 
-        self.admin_service.add_book(self.book)
-        self.admin_service.edit_book(self.book, title="Wiersze")
-        self.assertEqual(self.book.title, "Wiersze")
-        print(f"[PASS] test_edit_book_title: Book title changed to '{self.book.title}' successfully.")
+        print("\n[TEST CASE FOR: EDIT BOOK] test_edit_books_title: Adding book and editing its title.") 
+        book_to_edit = self.book_service.books[1]
+        self.admin_service.edit_book(book_to_edit, title="Title Edited")
+        self.assertEqual(book_to_edit.title, "Title Edited")
+        print(f"[PASS] test_edit_book_title: Book title changed to '{book_to_edit.title}' successfully.")
 
     def test_edit_books_author(self):
-        print("\n[TEST CASE] test edit_books_author: Editing books author.")
-        self.admin_service.add_book(self.book)
-        self.admin_service.edit_book(self.book, author="Juliusz Słowacki")
-        self.assertEqual(self.book.author, "Juliusz Słowacki")
-        print(f"[PASS] test_edit_book_author: Book author changed to '{self.book.author}' successfully.")
+        print("\n[TEST CASE FOR: EDIT BOOK] edit_books_author: Editing books author.")
+        book_to_edit = self.book_service.books[1]
+        self.admin_service.edit_book(book_to_edit, author="Edited Author")
+        self.assertEqual(book_to_edit.author, "Edited Author")
+        print(f"[PASS] test_edit_book_author: Book author changed to '{book_to_edit.author}' successfully.")
 
     def test_edit_book_not_found(self):
-        print("\n[TEST CASE] test_edit_book_not_found: Trying to edit book which is not present in the library.")
+        print("\n[TEST CASE FOR: EDIT BOOK] edit_book_not_found: Trying to edit book which is not present in the library.")
+        self.book = Book(author="Adam Mickiewicz", title="Pan Tadeusz", year=1834)
         self.assertRaises(exc.BookNotFoundError, self.admin_service.edit_book, self.book, title="New Title")
         print(f"[PASS] test_edit_book_not_found: Exception {exc.BookNotFoundError} raised properly.")
 
+
 # [TESTS FOR: DELETE A BOOK]
     def test_delete_book(self):
-        print("\n[TEST CASE] test_delete_book: Deleting the book from the library")
-        self.admin_service.add_book(self.book)
-        self.admin_service.delete_book(self.book)
-        print(f"[PASS] test_delete_book: Book '{self.book.title}' by {self.book.author} deleted from the library.")
+        print("\n[TEST CASE FOR: DELETE A BOOK] delete_book: Deleting the book from the library")
+        book_to_delete = self.book_service.books[2]
+        self.admin_service.delete_book(book_to_delete)
+        print(f"[PASS] test_delete_book: Book '{book_to_delete.title}' by {book_to_delete.author} deleted from the library.")
 
 
 # [TESTS FOR: SEARCH A BOOK]
-    # def test_search_by_author_substring(self):
-    #     # This test expects substring matching for author names (e.g., "Mickiewicz" matches "Adam Mickiewicz")
-    #     print("\n[TEST CASE] test_search_by_author_substring: Searching book in the full library by partial author name")
-    #     self.admin_service.add_book(self.book)
-    #     self.reader_service.books = self.admin_service.books
-    #     results = self.reader_service.search_books(author="Mickiewicz")
-    #     self.assertIsInstance(results, list)
-    #     self.assertIn(self.book, results)
-    #     print(f"[PASS] test_search_by_author_substring: Searching for a book by partial author name and receiving results successfully.")
-    #     print(f"[PASS] test_search_by_author: Book found by author search: '{self.book.title}' by {self.book.author}.")
-    #     print(f"[PASS] test_search_by_author: Searching for a book by author and receiving results successfully.")
+    def test_search_by_keyword(self):
+        print("\n[TEST CASE FOR: SEARCH A BOOK] search_by_keyword: Search book by title, author or year")
+        book_to_find = self.book_service.books[0]
+        search_phrase = self.reader_service.search_books(searched_term="Harry")
+        self.assertIsInstance(search_phrase, list)
+        self.assertIn(book_to_find, search_phrase)
+        print(f"[PASS] The book {book_to_find.title} by {book_to_find.author} has been found.")
 
-    def test_search_by_title_or_keyword(self):
-        print("\n[TEST CASE] test_search_by_title_or_keyword: Searching for a book by entering the full book title or by keyword")
-        self.admin_service.add_book(self.book)
-        self.reader_service.books = self.admin_service.books
-        results = self.reader_service.search_books(title="Pan")
-        self.assertIsInstance(results, list)
-        self.assertIn(self.book, results)
-        # Also test searching by the full book title
-        full_title_results = self.reader_service.search_books(title="Pan Tadeusz")
-        self.assertIsInstance(full_title_results, list)
-        self.assertIn(self.book, full_title_results)
-        print(f"[PASS] test_search_by_title_or_keyword: Book {self.book.title} found and results received successfully")
-
-    def test_search_no_results(self):
-        print("\n[TEST CASE] test_search_no_results: No matching books to searched phrase.")
-        self.admin_service.add_book(self.book)
-        self.reader_service.books = self.admin_service.books
-        results = self.reader_service.search_books(match="Whatever")
-        self.assertIsInstance(results, list)
-        self.assertEqual(len(results), 0)
-        self.assertRaises(exc.BookNotFoundError, self.reader_service.search_books, title="Whatever")
-        print(f"[PASS] test_search_no_results: No matching books found, empty list returned successfully.\nException {exc.BookNotFoundError} raised properly.")
-
-
+    def test_no_search_results(self):
+        print("\n[TEST CASE FOR: SEARCH A BOOK] no_search_result: No result for searching phrase while user's searching.")
+        search_phrase  = self.reader_service.search_books(searched_term="Whatever")
+        self.assertIsInstance(search_phrase, list)
+        self.assertEqual(len(search_phrase), 0)
+        print("[PASS] No book found, returning an empty list.")
 
 
 
