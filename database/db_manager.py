f23d58c3d1bcb529b9702cfe093be3a22b63aa42
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-09-14 13:07:58 +0200

    Created paths to json files with program data in config.py - module utils.The paths are needed to handle those json files services.

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,12 +1,18 @@
 # access to database, saving and reading library data from JSON file
 
+
+
+
+
+
+
 # 1. imports    2. function for all functions   3. Save to file function
 
 
 
-# read user from users list
-# read library from file
-# read list of loans
-# read user's list of loan
+# read user from users list - plik program_users.json
+# read library from file - plik the_awesome_library.json
+# read list of loans - plik list_of_loans.json
+# read user's list of loan - plik single_user_history.json
 
 # saving data in database
--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,15 @@
 # basic settings of the project like file paths, database parameters, etc.
+
+import os
+import json
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.resolve()
+DATA_DIR = BASE_DIR / "program_data"
+DATA_DIR.mkdir(exist_ok=True)   # If the catalog (program_data) exists, no action needed.
+
+LOANS_LIST_FILE_PATH = DATA_DIR / "list_of_loans.json"
+PROGRAM_USERS_FILE_PATH = DATA_DIR / "program_users.json"
+SINGLE_USER_LOANS_FILE_PATH = DATA_DIR / "single_user_history.json"
+THE_LIBRARY_FILE_PATH = DATA_DIR / "the_awesome_library.json"
+
