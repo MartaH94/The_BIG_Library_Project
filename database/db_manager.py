bfe7ce25b02e97c5bd447ee0b5f0ae0b07a58660
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-09-14 13:44:44 +0200

    Class JsonFIleService() has been created.
    Added methods to class, to handle all file services. Method to handle reading file and saving data in file are done.
    The next 4 methods have a placeholders for further develop in next step.

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,9 +1,32 @@
 # access to database, saving and reading library data from JSON file
 
+import json
+import utils.config as config
+from pathlib import Path
 
+class JsonFilesService():
+    def __init__(self, file_path: Path):
+        self.file_path = file_path
+        
+    def read_json_file(self):
+        with self.file_path.open("r", encoding="utf-8") as f:
+            return json.load(f)
 
+    def write_json_data(self, data):
+        with self.file_path.open("w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
 
+    def append_data_to_file(self):
+        pass
 
+    def update_file_data(self):
+        pass
+
+    def delete_data_from_file(self):
+        pass
+
+    def file_exists_checkout(self):
+        pass
 
 
 # 1. imports    2. function for all functions   3. Save to file function
