28156bc3599d0d4e736f2101405545ca3d98d09a
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-10-12 14:09:02 +0200

    Implement JSON file services for books, loans, and users with CRUD operations and error handling

--- tests/test_json_files_sevice.py
+++ tests/test_json_files_sevice.py
@@ -5,22 +5,23 @@ import tempfile
 import shutil
 import json
 
-from models import user
+from models.user import User
 from services.authorisation_service import UserAuthorisation
-import database.JSON_files_service as json_services
+import database.json_files_major_services as json_services
+
 from pathlib import Path
-from utils import config
+import utils.config as config
 import exceptions as exc
 
 class TestJsonServices(unittest.TestCase):
     def setUp(self):
         self.json_services = json_services.JsonFilesService()
 
-        self.admin_user = user.User(user_id="Marta.Boss", email="marta@mail.com", role="admin")
+        self.admin_user = User(user_id="Marta.Boss", email="marta@mail.com", role="admin")
         self.admin_authorisation = UserAuthorisation()
         self.admin_authorisation.login(self.admin_user)
 
-        self.reader_user = user.User(user_id="Andrzej.czytelnik", email="read@andrew.com", role="reader")
+        self.reader_user = User(user_id="Andrzej.czytelnik", email="read@andrew.com", role="reader")
         self.reader_authorisation = UserAuthorisation()
         self.reader_authorisation.login(self.reader_user)
 
