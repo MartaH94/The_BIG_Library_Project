cdc200f011b20b609d6efa9fdaafc58698d3a6f5
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-11-15 15:26:59 +0100

    Refactor user permissions structure: organize permissions into categories for improved clarity and management

--- utils/validators.py
+++ utils/validators.py
@@ -1,49 +1,156 @@
-# functions to validate user inputs, eg. login, credentials, etc. 
-# "librarian", "guest", "moderator"
+# functions to validate user's permissions 
 
 from models.user import User
 
 user_permissions = {
     "reader" : {
-        "borrow_book" : True,
-        "return_book" : True,
-        "book_the_book" : True,
-        "approve_reservations": True,
-        "view_books" : True,
-        "view_borrow_history": True,
-        "add_book" : True,
-        "edit_book" : False,
-        "delete_book" : False,
-        "search_book" : True,
-        "manage_users" : False,
-        "update_account": False,
-        "delete_data" : False,
-        "edit_data" : False
+        "books": {
+            "borrow_book" : True,
+            "return_book" : True,
+            "reserve_book" : True,
+            "add_book" : False,
+            "edit_book" : False,
+            "delete_book" : False,
+            "search_book" : True,
+            "view_books" : True
+        },
+
+        "account": {
+            "update_account": False,
+            "update_own_data": True,
+            "reset_password": False,
+            "view_borrow_history": True
+        },
+
+        "actions": {
+            "approve_reservations": False,
+            "manage_users" : False,
+            "view_logs": False,
+            "generate_reports": False,
+            "delegate_permissions": False,
+            "delete_data" : False,
+            "edit_data" : False
+        }
     },
+
     "admin": {
-        "borrow_book" : False,
-        "return_book" : False,
-        "book_the_book" : False,
-        "approve_reservations": True,
-        "view_books" : True,
-        "view_borrow_history": True,
-        "add_book" : True,
-        "edit_book" : True,
-        "delete_book" : True,
-        "search_book" : True,
-        "manage_users" : True,
-        "update_account": True,
-        "delete_data" : True,
-        "Edit_data" : True
+        "books": {
+            "borrow_book" : False,
+            "return_book" : False,
+            "reserve_book" : True,
+            "add_book" : True,
+            "edit_book" : True,
+            "delete_book" : True,
+            "search_book" : True,
+            "view_books" : True
+        },
+
+        "account": {
+            "update_account": True,
+            "update_own_data": True,
+            "reset_password": True,
+            "view_borrow_history": True
+        },
+
+        "actions": {
+            "approve_reservations": False,
+            "manage_users" : True,
+            "view_logs": True,
+            "generate_reports": True,
+            "delegate_permissions": True,
+            "delete_data" : True,
+            "edit_data" : True
+        }    
     },
+
     "librarian": {
-        "borrow_book": False
+        "books": {
+            "borrow_book" : False,
+            "return_book" : False,
+            "reserve_book" : True,
+            "add_book" : True,
+            "edit_book" : True,
+            "delete_book" : True,
+            "search_book" : True,
+            "view_books" : True
+        },
+
+        "account": {
+            "update_account": True,
+            "update_own_data": True,
+            "reset_password": False,
+            "view_borrow_history": True
+        },
+
+        "actions": {
+            "approve_reservations": True,
+            "manage_users" : False,
+            "view_logs": False,
+            "generate_reports": True,
+            "delegate_permissions": False,
+            "delete_data" : True,
+            "edit_data" : True
+        }
     },
+
     "guest": {
-        "borrow_book": False
+        "books": {
+            "borrow_book" : False,
+            "return_book" : False,
+            "reserve_book" : True,
+            "add_book" : False,
+            "edit_book" : False,
+            "delete_book" : False,
+            "search_book" : True,
+            "view_books" : True
+        },
+
+        "account": {
+            "update_account": False,
+            "update_own_data": False,
+            "reset_password": False,
+            "view_borrow_history": False
+        },
+
+        "actions": {
+            "approve_reservations": False,
+            "manage_users" : False,
+            "view_logs": False,
+            "generate_reports": False,
+            "delegate_permissions": False,
+            "delete_data" : False,
+            "edit_data" : False
+        }
     },
+
     "moderator": {
-        "borrow_book": False
+        "books": {
+            "borrow_book" : False,
+            "return_book" : False,
+            "reserve_book" : False,
+            "add_book" : True,
+            "edit_book" : True,
+            "delete_book" : False,
+            "search_book" : True,
+            "view_books" : True
+        },
+
+        "account": {
+            "update_account": False,
+            "update_own_data": True,
+            "reset_password": False,
+            "view_borrow_history": True
+        },
+
+        "actions": {
+            "approve_reservations": False,
+            "manage_users" : False,
+            "view_logs": True,
+            "generate_reports": True,
+            "delegate_permissions": False,
+            "delete_data" : False,
+            "edit_data" : True
+        }
     }
 }
 
