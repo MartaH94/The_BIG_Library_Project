ba44f4a43930e4a03a26f26b9bbcff7257d3a8ce
MartaH94 <103372407+MartaH94@users.noreply.github.com>
2025-11-15 13:34:57 +0100

    Refactor database_schemes.py: enhance documentation and improve schema structure for clarity and consistency

--- database/database_schemes.py
+++ database/database_schemes.py
@@ -1,3 +1,47 @@
+""" This module defines the database schemas for the library management system. 
+    Each schema is represented as a dictionary where keys are field names and values are their expected data types."""
+
+from models.user import valid_roles
+from datetime import date, datetime
+""" date - if you need only the date, e.g.: date of book return (YYYY-MM-DD)
+    datetime - if you need an hour too. """
+
+# In validation implement file format convertion frm text file to type date / datetime
+# and checking corectness of file's format
+
+database_schemes = {
+    "User" : {
+        "id": int,
+        "role": list(valid_roles),
+        "is_active": bool,
+        "last_login": datetime,
+
+        "profile": {
+            "user_name": str,
+            "first_name": str,
+            "last_name": str,
+            "email": str,
+            "phone_number": int,
+            "password_hash": str
+        }
+    },
+
+    "Book":{
+        "id": int,
+        "title": str,
+        "author": str,
+        "isbn": str,
+        "publication_year": int,
+        "category": str,
+        "language": str,
+        "book_status": str,
+        "borrower_id": str,
+        "due_date": date
+    }
+
+
+
+}
 
 
 userSchema = {"id": int, "name": str, "email": "str", "role": str}
