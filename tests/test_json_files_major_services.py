6fd6d6136981499991a31ec85dba461bdb1dd376
Marta Haraj <marta.haraj@accenture.com>
2026-03-23 13:46:49 +0100

    Introduced test script for json_files_major_services.py
    Prepared needed imports. Edits in docsrings to keep the information updated.

---
+++ tests/test_json_files_major_services.py
@@ -0,0 +1,43 @@
+"""
+________________________________________________________
+tests.test_json_files_service
+========================================================
+Test Class for json_files_major_services module.
+________________________________________________________
+
+TO DO HERE:
+- Create unit tests for all methods in json_files_major_services module.
+
+Test cases should cover behaviors for all methods, including edge cases and error handling.
+
+List of tests to implement:
+- test_checking_file_exists
+- test_loading_json_file
+- test_validate_file_data
+- test_create_backup_file
+- test_remove_from_file
+- test_update_data_in_file
+- test_write_json_data
+- test_get_or_create_backup_dir
+- test_build_backup_file_name
+- test_check_file_returns_correct_data
+- test_exceptions_handling_in_methods
+
+
+"""
+
+
+import json
+import tempfile
+import unittest
+from pathlib import Path
+from unittest import TestCase
+
+import exceptions as exc
+import utils.config as config
+from database.json_files_major_services import JsonFilesService
+from models.user import User
+from services.authorisation_service import UserAuthorisation
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
