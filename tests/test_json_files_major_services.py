97c6b20977c421644e8e382ecd72dbcd4d6bda82
Marta Haraj <marta.haraj@accenture.com>
2026-03-24 12:06:56 +0100

    Replaced assert in test_creates_file_when_missing to correctly verify type of data

--- tests/test_json_files_major_services.py
+++ tests/test_json_files_major_services.py
@@ -58,7 +58,7 @@ class TestMethodFileExistsChecking(unittest.TestCase):  # 4
         with open(file_to_check, encoding="utf-8") as f:
             file_content = json.load(f)
 
-        self.assertEqual(file_content, list)
+        self.assertIsInstance(file_content, list)
 
     def test_initializes_list_in_empty_file(self):
         self.json_service = JsonFilesService(file_path=self.path_to_empty_file)
