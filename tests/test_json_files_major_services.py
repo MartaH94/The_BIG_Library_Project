f8928ede343a013801e8547b235bf3ed5355f5d8
Marta Haraj <marta.haraj@accenture.com>
2026-03-26 13:06:12 +0100

    test case: reads_existing_list_from_file fully implemented
    Tested method: load_json_file

--- tests/test_json_files_major_services.py
+++ tests/test_json_files_major_services.py
@@ -100,6 +100,7 @@ class TestMethodLoadJsonFile(unittest.TestCase):  # 4
         self.empty_json_file_path = self.temporary_dir_path / "empty_file.json"
         self.missing_json_file_path = self.temporary_dir_path / "missing_file.json"
 
+        """ Service under test """
         self.load_service = JsonFilesService(file_path=self.valid_json_file_path)
 
         """ load_json_file expects a list in valid JSON file """
@@ -116,10 +117,15 @@ class TestMethodLoadJsonFile(unittest.TestCase):  # 4
 
     def test_reads_existing_list_from_file(self):
         self.valid_json_file = self.valid_json_file_path
+
         with self.valid_json_file.open("w", encoding="utf-8") as f:
             json.dump(self.valid_file_data, f)
 
-        self.load_service.load_json_file()
+        self.load_service.file_path = self.valid_json_file_path
+
+        test_result = self.load_service.load_json_file()
+
+        self.assertEqual(test_result, self.valid_file_data)
 
     def test_raises_file_error_if_json_doesnt_contain_list(self):
         pass
