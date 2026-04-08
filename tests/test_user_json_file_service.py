89521efd4c6ccb73d5b28579aecbf2ccb4624441
Marta Haraj <marta.haraj@accenture.com>
2026-04-08 14:13:05 +0200

    Prepared test classes and prepared placeholders for test classes

--- tests/test_user_json_file_service.py
+++ tests/test_user_json_file_service.py
@@ -5,7 +5,7 @@ tests.test_user_json_file_service.py
 Test for the file user_json_file_service.py
 ________________________________________________________
 
-Test classes:
+Test classes: 5
 Test cases total:
 
 current status: not started
@@ -16,26 +16,53 @@ Total number of done test cases:
 import json
 import tempfile
 import unittest
+from pathlib import Path
 
 import exceptions as exc
+from database.loan_json_file_service import LoanJsonFileService
 from database.user_json_file_service import UsersJsonFileService
 
 
 class TestUserServiceGetUserData(unittest.TestCase):
+    """Method under test:
+    Number of TestCases:
+    Done TestCases:
+    """
+
     pass
 
 
 class TestUserServiceAddUserData(unittest.TestCase):
+    """Method under test:
+    Number of TestCases:
+    Done TestCases:
+    """
+
     pass
 
 
 class TestUserServiceGetAllUsersList(unittest.TestCase):
+    """Method under test:
+    Number of TestCases:
+    Done TestCases:
+    """
+
     pass
 
 
 class TestUserServiceUpdateUserData(unittest.TestCase):
+    """Method under test:
+    Number of TestCases:
+    Done TestCases:
+    """
+
     pass
 
 
 class TestUserServiceDeleteUserById(unittest.TestCase):
+    """Method under test:
+    Number of TestCases:
+    Done TestCases:
+    """
+
     pass
