030b25c1a467245a1861704e113be563b883eefb
Marta Haraj <marta.haraj@accenture.com>
2026-04-08 09:55:38 +0200

    Introduced test cases for each testing class.

--- tests/test_book_json_file_service.py
+++ tests/test_book_json_file_service.py
@@ -23,6 +23,18 @@ from database.book_json_file_service import BookJsonFileService
 
 class TestBookJsonFileServiceGetBookData(unittest.TestCase):
     def setUp(self):
+        self.temporary_dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.temporary_dir.cleanup()
+
+    def test_raises_validation_error_when_book_id_is_none(self):
+        pass
+
+    def test_returns_book_when_book_id_exists(self):
+        pass
+
+    def test_raises_book_not_found_error_when_book_id_not_found(self):
         pass
 
 
@@ -33,6 +45,21 @@ class TestBookJsonFileServiceAddBookData(unittest.TestCase):
     def tearDown(self):
         self.temporary_dir.cleanup()
 
+    def test_raises_validation_error_when_book_data_is_missing(self):
+        pass
+
+    def test_raises_data_type_error_when_book_data_is_not_dict(self):
+        pass
+
+    def test_raises_book_error_when_book_id_already_exists(self):
+        pass
+
+    def tets_raises_book_validation_error_when_schema_validation_fails(self):
+        pass
+
+    def test_writes_json_and_returns_success_message_when_data_is_valid(self):
+        pass
+
 
 class TestBookJsonFileServiceGetAllBooksList(unittest.TestCase):
     def setUp(self):
@@ -40,6 +67,12 @@ class TestBookJsonFileServiceGetAllBooksList(unittest.TestCase):
 
     def tearDown(self):
         self.temporary_dir.cleanup()
+
+    def test_returns_all_books_when_database_is_not_empty(self):
+        pass
+
+    def test_raises_book_not_found_error_when_database_is_empty(self):
+        pass
 
 
 class TestBookJsonFileServiceUpdateBookData(unittest.TestCase):
@@ -49,6 +82,24 @@ class TestBookJsonFileServiceUpdateBookData(unittest.TestCase):
     def tearDown(self):
         self.temporary_dir.cleanup()
 
+    def test_raises_validation_error_when_book_id_is_none(self):
+        pass
+
+    def test_raises_validation_error_when_field_is_none(self):
+        pass
+
+    def test_raises_validation_error_when_new_value_is_none(self):
+        pass
+
+    def test_raises_validation_error_when_field_not_in_book(self):
+        pass
+
+    def test_raises_book_not_found_error_when_book_id_not_found(self):
+        pass
+
+    def test_updates_book_and_writes_json_when_data_is_valid(self):
+        pass
+
 
 class TestBookJsonFileServiceDeleteBookById(unittest.TestCase):
     def setUp(self):
@@ -56,3 +107,12 @@ class TestBookJsonFileServiceDeleteBookById(unittest.TestCase):
 
     def tearDown(self):
         self.temporary_dir.cleanup()
+
+    def test_raises_validation_error_when_book_id_is_none(self):
+        pass
+
+    def test_raises_book_error_when_book_id_not_found(self):
+        pass
+
+    def test_removes_book_and_writes_json_when_book_id_exists(self):
+        pass
