34a619149f485b16e1910069ba9efc079833234e
Marta Haraj <marta.haraj@accenture.com>
2026-04-27 15:16:24 +0200

    fix: update test files to standardize TODO comments and remove outdated instructions

--- tests/test_book_json_file_service.py
+++ tests/test_book_json_file_service.py
@@ -12,9 +12,9 @@ current status: in progress
 Total number of done test cases: 3
 
 
-IMPORTANT:
-book_schema (database_schemes.py) requires rebuilding to Json schema style to prepare required fields. Otherwise all fields must be filles during adding book data.
-Verify test cases after that changes once they are implemented.
+TO DO:
+Verify existing test cases
+Add new test cases if needed
 """
 
 import json
